4ef880747b64329052341bfb370d5c626e35523c
unknown <moogieee@ODSPR10234W45.odsdai.netdai.com>
2017-07-28 19:58:42 -0700

    finished!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,10 +19,14 @@ $("#add-train-btn").on("click", function(event) {
 	// Grab user input
 	var trainName = $("#train-name-input").val().trim();
 	var destination = $("#destination-input").val().trim();
-	var firstTrainTime = $("#first-train-time-input").val().trim();
+	// var firstTrainTime = $("#first-train-time-input").val().trim();
+
+	// store firstTrainTime as UNIX format
+	var firstTrainTime = moment($("#first-train-time-input").val().trim(), "HH:mm").format("X");
+	
 	var frequency = $("#frequency-input").val().trim();
 
-	// create local "temporary" object for holding employee data
+	// create local "temporary" object for holding train data
 	var newTrain = {
 		name: trainName,
 		destination: destination,
@@ -30,16 +34,16 @@ $("#add-train-btn").on("click", function(event) {
 		frequency: frequency
 	};
 
-	// upload employee data to the database
+	// upload train data to the database
 	database.ref().push(newTrain);
 
-	console.log(newTrain.name);
-	console.log(newTrain.destination);
-	console.log(newTrain.firstTrainTime);
-	console.log(newTrain.frequency);
+	// console.log(newTrain.name);
+	// console.log(newTrain.destination);
+	// console.log(newTrain.firstTrainTime);
+	// console.log(newTrain.frequency);
 
 
-	alert("Train schedule successfully added");
+	alert("Train successfully added");
 
 	// clear the text boxes
 	$("#train-name-input").val("");
@@ -50,7 +54,6 @@ $("#add-train-btn").on("click", function(event) {
 });
 
 database.ref().on("child_added", function(snapshot, prevChildKey) {
-
 	console.log(snapshot.val());
 
 	var name = snapshot.val().name;
@@ -58,18 +61,46 @@ database.ref().on("child_added", function(snapshot, prevChildKey) {
 	var firstTrainTime = snapshot.val().firstTrainTime;
 	var frequency = snapshot.val().frequency;
 
-	var remainingTime = moment().diff(moment.unix(firstTrainTime), "minutes") % frequency;
 
-	var nextArrival = moment().add(minutesAway,"m").format("hh:mm A");
+// =================== Train Time Calculations ====================
 
-	var minutesAway = frequency - remainingTime;
+
+	// format currentTime to military time
+	var currentTime = moment().format("HH:mm");
+	console.log("Current Time: " + currentTime);
+
+	// calculate difference betweent currentTime & trainArrival in "minutes"
+	var timeDifference = moment().diff(moment.unix(firstTrainTime, "X"), "minutes");
+	console.log("Time Difference: " + timeDifference);
+
+	// divide diffTime by frequency to get the remainder
+	var timeRemainder = timeDifference % frequency;
+	console.log("Time Remainder: " + timeRemainder);
+
+	// minTilArrival = frequency - timeRemainder
+	var minTilArrival = frequency - timeRemainder;
+	console.log("Minutes Until Arrival: " + minTilArrival);
+
+	// nextTrainArrival = minTilArrival + currentTime
+	var nextTrainArrival = moment().add(minTilArrival, "minutes").format("HH:mm");
+	console.log("Next Train Arrives in: " + nextTrainArrival);
+
+
+
+//==================================================================
+
 
 	console.log(name);
 	console.log(destination);
 	console.log(firstTrainTime);
 	console.log(frequency);
-	console.log(nextArrival);
-	console.log(minutesAway);
+	console.log(nextTrainArrival);
+	console.log(minTilArrival);
 
-	$("#train-table > tbody").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + " min" + "</td></tr>");
+	$("#train-table > tbody").append("<tr><td>" + name + 
+									"</td><td>" + destination + 
+									"</td><td>" + frequency + 
+									"</td><td>" + nextTrainArrival + 
+									"</td><td>" + minTilArrival + 
+									" min" + "</td></tr>");
 });
